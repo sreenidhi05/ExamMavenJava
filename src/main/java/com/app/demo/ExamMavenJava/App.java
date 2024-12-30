efa65f368aeaa4852fb8c337b7b51fe7766a4e05
sreenidhi <sarojasreenidhi@gmail.com>
2024-12-30 11:26:38 +0530

    Mid Lab Exam-Maven Java Project

---
+++ src/main/java/com/app/demo/ExamMavenJava/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.ExamMavenJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "SE Internal Lab" );
+    }
+}
